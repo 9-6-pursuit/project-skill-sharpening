ac084e2b1837bb2ea078da9e19fada8c6d835824
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-01-22 20:41:37 -0500

    Finished my project

--- src/06-loops.js
+++ src/06-loops.js
@@ -11,8 +11,17 @@
  *  //> 25
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
-function rangeSum(min, max) {}
+function rangeSum(min, max) {
+  let fullArr = [];
+  let sum = 0;
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
+  for (let i = min; i <= max; i++) {
+      fullArr.push(i);
+}
+  sum = fullArr.reduce(reducer);
+      return sum;
+}
 /**
  * rangeOdd()
  * ---------------------
@@ -25,7 +34,31 @@ function rangeSum(min, max) {}
  *  rangeOdd(10, 15);
  *  //> [ 15, 13, 11 ]
  */
-function rangeOdd(min, max) {}
+function rangeOdd(min, max){
+  let myArray = [];
+//   if (max % 2 !== 0){
+//   for (let i = max; i >= min; i -=2){
+//     myArray.push(i);
+//   }
+// } else if (max % 2 === 0)
+// {max--
+//   for (let i = max; i >= min; i -=2){
+//     myArray.push(i);
+//   }
+// }
+//    ANOTHER WAY TO DO IT WITHOUT WRITING ANOTHER CONDITIONAL
+   if (max % 2 === 0){
+      max--
+    for (let i = max; i >= min; i -=2){
+      myArray.push(i);
+  }
+  } else {
+    for (let i = max; i >= min; i -=2){
+      myArray.push(i);
+  }
+  }
+    return myArray
+  } 
 
 /**
  * rangeEveryOther()
@@ -39,7 +72,13 @@ function rangeOdd(min, max) {}
  *  rangeEveryOther(11, 18);
  *  //> [ 11, 13, 15, 17 ]
  */
-function rangeEveryOther(min, max) {}
+function rangeEveryOther(min, max) {
+  myArray = []
+  for (let i = min; i <= max; i +=2){
+    myArray.push(i);
+} 
+    return myArray
+}
 
 /**
  * containsWhileLoop()
@@ -61,8 +100,15 @@ function rangeEveryOther(min, max) {}
  *  containsWhileLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsWhileLoop(array, target) {}
-
+function containsWhileLoop(array, target) {
+  let i = 0
+  while (i <array.length){
+   if (array.indexOf(target) != -1){
+   return true 
+   } i++
+   }return false
+  }
+ 
 /**
  * containsForLoop()
  * ---------------------
@@ -83,7 +129,13 @@ function containsWhileLoop(array, target) {}
  *  containsForLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsForLoop(array, target) {}
+function containsForLoop(array, target){
+  for (const element of array){
+     if (element === target)  {
+        return true 
+  }
+  }     return false
+  } 
 
 /**
  * targetCount()
@@ -97,7 +149,9 @@ function containsForLoop(array, target) {}
  *  targetCount([ 10, 20, 10, 20, 30 ], 10);
  *  //> 2
  */
-function targetCount(array, target) {}
+function targetCount(array, target){
+  return array.filter((t) => (t === target)).length;
+}
 
 /**
  * firstIndexFound()
@@ -117,7 +171,13 @@ function targetCount(array, target) {}
  *  firstIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function firstIndexFound(array, target) {}
+function firstIndexFound(array, target) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === target) {
+          return i;
+  }
+  }       return -1
+  }
 
 /**
  * lastIndexFound()
@@ -137,8 +197,9 @@ function firstIndexFound(array, target) {}
  *  lastIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function lastIndexFound(array, target) {}
-
+function lastIndexFound(array, target) {
+  return array.lastIndexOf(target, array.length-1)
+}
 /**
  * timesIndex()
  * ---------------------
@@ -151,8 +212,15 @@ function lastIndexFound(array, target) {}
  *  //> [ 7 * 0, 10 * 1, 11 * 2 ]
  *  //> [ 0, 10, 22 ]
  */
-function timesIndex(array) {}
-
+function timesIndex(array) { 
+  //var numbers = [1,2,3]; 
+  let product = []; 
+  for(let i = 0; i < array.length; i++){
+      product.push((i*array[i]));
+}
+      return product;
+}
+  
 /**
  * cumulativeSum()
  * ---------------------
@@ -165,7 +233,20 @@ function timesIndex(array) {}
  *  //> [ 5, 5 + 2, 5 + 2 + 9 ]
  *  //> [ 5, 7, 16 ]
  */
-function cumulativeSum(array) {}
+function cumulativeSum(array) {
+ // var numbers = [1,2,3]; 
+// let sum = []; 
+// for(let i = 0;i < array.length; i++){
+//     sum.push((i+=array[i]));
+// }
+// return sum;
+
+// Adds all the numbers together in the array
+for (let i = 1; i < array.length; i++){
+    array[i] = array[i]+array[i-1]
+}
+    return array;
+}
 
 // Do not change anything below this line.
 module.exports = {
--- src/08-accessing.js
+++ src/08-accessing.js
@@ -32,7 +32,9 @@
  *  getFirstName(person);
  *  //> "Rachel"
  */
-function getFirstName(person) {}
+function getFirstName(person) {
+  return person.names.first
+}
 
 /**
  * getLastName()
@@ -45,7 +47,9 @@ function getFirstName(person) {}
  *  getLastName(person);
  *  //> "Rojas"
  */
-function getLastName(person) {}
+function getLastName(person) {
+  return person.names.last
+}
 
 /**
  * getAddressStreet()
@@ -58,7 +62,9 @@ function getLastName(person) {}
  *  getAddressStreet(person);
  *  //> "697 Pine Drive"
  */
-function getAddressStreet(person) {}
+function getAddressStreet(person) { 
+  return person.address.street
+}
 
 /**
  * getCountOfPhoneNumbers()
@@ -71,7 +77,9 @@ function getAddressStreet(person) {}
  *  getCountOfPhoneNumbers(person);
  *  //> 2
  */
-function getCountOfPhoneNumbers(person) {}
+function getCountOfPhoneNumbers(person) {
+  return person.numbers.length
+}
 
 /**
  * getFirstPhoneNumber()
@@ -87,7 +95,9 @@ function getCountOfPhoneNumbers(person) {}
  *  getFirstPhoneNumber(person);
  *  //> 7185550921
  */
-function getFirstPhoneNumber(person) {}
+function getFirstPhoneNumber(person) {
+  return person.numbers[0]
+}
 
 /**
  * getLastPhoneNumber()
@@ -103,7 +113,9 @@ function getFirstPhoneNumber(person) {}
  *  getLastPhoneNumber(person);
  *  //> 7185558611
  */
-function getLastPhoneNumber(person) {}
+function getLastPhoneNumber(person) {
+  return person.numbers[person.numbers.length-1]
+}
 
 /**
  * getFullName()
@@ -116,7 +128,12 @@ function getLastPhoneNumber(person) {}
  *  getFullName(person);
  *  //> "Rachel Eleanor Rojas"
  */
-function getFullName(person) {}
+function getFullName(person) {
+  let first = person.names.first
+  let middle = person.names.middle
+  let last = person.names.last
+  return `${first} ${middle} ${last}`
+}
 
 /**
  * getCityAndState()
@@ -132,7 +149,11 @@ function getFullName(person) {}
  *  getCityAndState(person);
  *  //> "Staten Island, NY"
  */
-function getCityAndState(person) {}
+function getCityAndState(person) {
+    let city = person.address.city
+    let state = person.address.state
+  return `${city}, ${state}`
+}
 
 /**
  * getFullAddress()
@@ -148,7 +169,14 @@ function getCityAndState(person) {}
  *  getFullAddress(person);
  *  //> "697 Pine Drive 2A, Staten Island, NY, 10306"
  */
-function getFullAddress(person) {}
+function getFullAddress(person) {
+    let street = person.address.street
+    let unit = person.address.unit
+    let city = person.address.city
+    let state = person.address.state
+    let zip = person.address.zip
+  return `${street} ${unit}, ${city}, ${state}, ${zip}`
+}
 
 /**
  * getFlatObject()
@@ -171,7 +199,52 @@ function getFullAddress(person) {}
       numbers: [7185550921, 7185558611],
     };
  */
-function getFlatObject(person) {}
+function getFlatObject(person) {
+  let flat = {}
+    
+    flat.first = person.names.first
+    flat.middle = person.names.middle
+    flat.last = person.names.last
+    flat.street = person.address.street
+    flat.unit = person.address.unit
+    flat.city = person.address.city
+    flat.state = person.address.state
+    flat.zip = person.address.zip 
+    flat.numbers = person.numbers
+    // console.log(person.numbers)
+    // console.log()
+  return flat
+}
+  // console.log(flat)
+// //                RETURNS AS undefined
+
+
+//   let flat = {};
+ 
+//     // loop through the object "person"
+//     for (const i in person) {
+ 
+//         // We check the type of the i using
+//         // typeof() function and recursively
+//         // call the function again
+//         if ((typeof person[i]) === 'object') {
+//             const temp = getFlatObject(person[i]);
+//             for (const j in temp) {
+//               person[i] ===(person[i])
+ 
+//                 // Store temp in flat
+//                 flat[i] = temp[j];
+//             }
+//         }
+ 
+//         // Else store person[i] in flat directly
+//         else {
+//             flat[i] = person[i];
+//         }
+//     }
+//     return flat;
+
+
 
 // Do not change the code below.
 module.exports = {
