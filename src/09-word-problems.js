e69b266777308707af01bb10eaae60d85ae76adc
Sayje Essoka-Lasenberry <sayjeessoka@Sayjes-Air.lan>
2023-01-23 19:03:58 -0500

    Sharp

--- src/09-word-problems.js
+++ src/09-word-problems.js
@@ -19,7 +19,20 @@
  *  applyDiscount(1000, 9, true);
  *  //> 700
  */
-function applyDiscount(priceInCents, age, hasMembership) {}
+function applyDiscount(priceInCents, age, hasMembership) {
+  var discount = 0;
+  if (age <= 10 || age >= 65) {
+      discount += 0.1;
+  }
+  if (hasMembership) {
+      discount += 0.2;
+  }
+  if (discount > 0.2) {
+      discount = 0.3;
+  }
+  return priceInCents - (priceInCents * discount);
+}
+
 
 /**
  * getCartTotal()
@@ -40,7 +53,13 @@ function applyDiscount(priceInCents, age, hasMembership) {}
     getCartTotal(cart);
  *  //> "$30.00"
  */
-function getCartTotal(products) {}
+ function getCartTotal(products) {
+  let total = 0;
+  for (let i = 0; i < products.length; i++) {
+  total += products[i].price;
+  }
+  return total;
+ }
 
 /**
  * compareLocations()
@@ -80,7 +99,17 @@ function getCartTotal(products) {}
     compareLocations(address1, address2);
     //> "Same city."
  */
-function compareLocations(address1, address2) {}
+function compareLocations(address1, address2) {
+  if (address1.street === address2.street && address1.city === address2.city && address1.state === address2.state && address1.zip === address2.zip) {
+    return "Same building.";
+    } else if (address1.city === address2.city && address1.state === address2.state && address1.zip === address2.zip) {
+    return "Same city.";
+    } else if (address1.state === address2.state) {
+    return "Same state.";
+    } else {
+    return "Addresses are not near each other.";
+    }
+    }
 
 /**
  * gradeAssignments()
